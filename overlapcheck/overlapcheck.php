eeb28d2d96333449cb12d49a8748a22335fdfddb
Yujong <whdgus5474@naver.com>
2019-04-05 17:17:23 +0900

    overlapcheck.php

---
+++ overlapcheck/overlapcheck.php
@@ -0,0 +1,96 @@
+<?php
+include "../DBconnection/DBconnection.php";
+
+$separator = $_POST['emailOrnick'];
+
+$accountEmail = $_POST['accountEmail'];
+$accountNick = $_POST['accountNick'];
+
+
+if ($separator == 0)
+{
+
+    $email_check = "SELECT * FROM account WHERE accountEmail ='".$accountEmail."'";
+    $email_check_result = mysqli_query($connect,$email_check);
+    $email_check_result_row = mysqli_num_rows($email_check_result);
+
+
+    if($email_check_result_row == 0 )
+    {
+        $result = array
+        (
+            "result" => "true"
+        );
+    }
+    else
+    {
+        $result = array
+        (
+            "result" => "false"
+        );
+    }
+}
+else
+{
+    $nick_check = "SELECT * FROM account WHERE accountNick ='".$nick."'";
+    $nick_check_result = mysqli_query($connect,$nick_check);
+    $nick_check_result_row = mysqli_num_rows($nick_check_result);
+
+
+    if($nick_check_result_row == 0)
+    {
+        $result = array
+        (
+            "result" => "true"
+        );
+    }
+    else
+    {
+        $result = array
+        (
+            "result" => "false"
+        );
+    }
+}
+
+echo json_encode($result);
+
+//
+//function Emailcheck($email,$connect)
+//{
+//    $email_check = "SELECT * FROM account WHERE accountEmail ='".$email."'";
+//    $email_check_result = mysqli_query($connect,$email_check);
+//    $email_check_result_row = mysqli_num_rows($email_check_result);
+//
+//    if ($email_check_result_row == 0)
+//    {
+//        $return = true;
+//    }
+//    else
+//    {
+//        $return = false;
+//    }
+//
+//    return $return;
+//}
+//function Nickcheck($nick,$connect)
+//{
+//    $nick_check = "SELECT * FROM account WHERE accountNick ='".$nick."'";
+//    $nick_check_result = mysqli_query($connect,$nick_check);
+//    $nick_check_result_row = mysqli_num_rows($nick_check_result);
+//
+//    if ($nick_check_result_row == 0)
+//    {
+//        $return = true;
+//    }
+//    else
+//    {
+//        $return = false;
+//    }
+//
+//    return $return;
+//}
+
+?>
+
+
