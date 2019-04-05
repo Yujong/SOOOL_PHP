f03bb180edb8eada0ace83173b049ad2daa16e43
Yujong <whdgus5474@naver.com>
2019-04-05 16:41:27 +0900

    ttest.php

---
+++ Signup/ttest.php
@@ -0,0 +1,59 @@
+<?php
+//
+//$result = array
+//(
+//    "result" => "true",
+//    "accountNo" => "123"
+//);
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+//
+//
+//$image_array = array($_FILES['accountImage']['name']);
+//$image_array_count = count($image_array);
+
+
+
+$image_save_dir = "../qnaImage";
+
+$image_save_name = $_FILES['description']['name'];
+
+$image_save_path = $image_save_dir ."/". $image_save_name;
+
+$image_tmp_name = $_FILES['description']['tmp_name'];
+
+move_uploaded_file("$image_tmp_name","$image_save_path");
+
+//for ($i = 0;$i < $image_array_count;$i++)
+//{
+//    $image_tmp_name = $_FILES['accountImage']['tmp_name'][$i];
+//
+//    if($image_tmp_name != "")
+//    {
+//        $image_save_dir = "../qnaImage";
+//
+//        $image_save_name = $_FILES['accountImage']['name'][$i];
+//
+//        $image_save_path = $image_save_dir ."/". $image_save_name;
+//
+//        if (move_uploaded_file("$image_tmp_name","$image_save_path"))
+//        {
+//            echo "ok";
+//        }
+//        else
+//        {
+//            echo $_FILES['accountImage']['error'];
+//        }
+//
+//    }
+//
+//}
+
+
+
+//
+//header('Content-Type: application/json');
+//echo json_encode(array($result));
+
+?>
